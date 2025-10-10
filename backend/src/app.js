9f9d17c39d03e113e5a78d4aec3b74bb38eb178d
Satyam Gupta <satyamgupta9302@gmail.com>
2025-10-10 20:08:14 +0530

    add gitignore

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,30 @@
 import express from "express";
 import {createServer} from "http"
-
+import dotenv from "dotenv"
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 
+
+dotenv.config();
+
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+
 
 app.get("/home",(req,res)=>{
     return res.json({"hello":"world"})
 })
 
 const start = async() =>{
-    app.listen(8000,()=>{
+
+    const connectionDB = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`connection db Host :${connectionDB.connection.host}`);
+    
+    const PORT = process.env.PORT || 8000;
+    server.listen(app.get("port"),()=>{
         console.log("Running on Port 8000")
     })
 }
