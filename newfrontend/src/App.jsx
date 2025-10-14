d40e38c6b42096f40407271140251dbe53379bbf
Satyam Gupta <satyamgupta9302@gmail.com>
2025-10-14 16:58:38 +0530

    upload new frontend

---
+++ newfrontend/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage.jsx';
+import Authentication from './pages/Authentication.jsx';
+// import { AuthProvider } from './contexts/AuthContext';
+import VideoMeet from './pages/VideoMeet';
+import Home from './pages/Home.jsx';
+import History from './pages/History.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        {/* Wrap routes with AuthProvider if needed */}
+        {/* <AuthProvider> */}
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/auth' element={<Authentication />} />
+          {/* <Route path='/home' element={<Home />} /> */}
+          {/* <Route path='/history' element={<History />} /> */}
+          {/* <Route path='/:url' element={<VideoMeet />} /> */}
+        </Routes>
+        {/* </AuthProvider> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
