01b1e99d7f45f05f83dc0b6bd2643a5662691c04
Satyam Gupta <satyamgupta9302@gmail.com>
2025-12-13 16:35:37 +0530

    controller for history add

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -104,7 +104,24 @@ const getUserHistory = async (req, res) => {
     }
 }
 
+const addToHistory = async (req, res) => {
+    const { token, meeting_code } = req.body;
 
+    try {
+        const user = await User.findOne({ token: token });
+
+        const newMeeting = new Meeting({
+            user_id: user.username,
+            meetingCode: meeting_code
+        })
+
+        await newMeeting.save();
+
+        res.status(httpStatus.CREATED).json({ message: "Added code to history" })
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
+    }
+}
 
 
 export {login,register}
